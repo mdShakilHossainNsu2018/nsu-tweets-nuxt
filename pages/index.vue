6b29cdca68e6c527f56f106c0969af4f705a9801
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-07-20 02:09:20 +0600

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+      <h1>Hello nsu</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data: function (){
+    return {
+      tweets: {}
+    }
+  },
+
+  mounted() {
+    this.$axios.get('http://127.0.0.1:8000/api/tweets/').then(res => {console.log(res.data)}).catch(
+      err => console.log(err)
+    )
+  }
+
+}
+</script>
+
+
+<style>
+
+</style>
